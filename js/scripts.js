1bb4656dcf28f45d6dcb30b20f36ff9f9bd1f2b4
Liz <51245710+eahowell@users.noreply.github.com>
2023-11-29 16:55:17 -0600

    Update: Created table of Pokemon
    Created a table and added a size classification based on the height

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Create the array of Pokemons
 let pokemonList = [];
 pokemonList = [
   { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
@@ -5,11 +6,48 @@ pokemonList = [
   { name: "Venusaur", height: 2, types: ["grass", "poison"] },
 ];
 console.log(pokemonList);
-document.write
+
+
+// Set up table header
+document.write(`
+    <table class=tablePokemonList>
+    <thead>
+        <tr>
+            <th> Name </th>
+            <th> Height (m) </th>
+            <th> Type </th>
+            <th> Size </th>
+        </tr>
+    </thead>
+    <tbody>
+    `);
+
+
+// Loop through array to put the information in a table
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(`
-    ${pokemonList[i].name}  
-    (Height: ${pokemonList[i].height}   
-     and Type: ${pokemonList[i].types})`);
-  document.write('<br>');
-};
+  document.write(`<tr>
+        <td>${pokemonList[i].name}</td>
+        <td>${pokemonList[i].height}</td>   
+        <td>${pokemonList[i].types}</td>
+        <td>
+        `);
+
+// Determine the size classification based on the height
+    if (pokemonList[i].height < 1) {
+        document.write("Tiny");
+    } else if (pokemonList[i].height >= 1 && pokemonList[i].height <= 1.9) {
+        document.write("Medium");
+    } else if (pokemonList[i].height > 1.9) {
+        document.write("Large");
+    } else {
+        document.write("Unclassified");
+    }
+
+    document.write(`
+            </td>  
+        </tr>`);
+}
+    document.write(`
+        </tbody>
+        </table>
+    `);
