aac7545dbc097d0524819e66b66be1021add1f7f
Liz <51245710+eahowell@users.noreply.github.com>
2023-11-29 14:42:06 -0600

    Feat: Added a print of Pokemon in the console log and the html

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ pokemonList = [
   { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
   { name: "Venusaur", height: 2, types: ["grass", "poison"] },
 ];
+console.log(pokemonList);
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`
+    ${pokemonList[i].name}  
+    ${pokemonList[i].height}  
+    ${pokemonList[i].types} 
+    ----`);
+  }
