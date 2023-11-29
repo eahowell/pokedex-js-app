054ca5f65d2835219692d923f81c8801c3fff317
Liz <51245710+eahowell@users.noreply.github.com>
2023-11-29 14:23:57 -0600

    Feat: Adding an array of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert("Hello world");
-
-let favoriteFood = "Ice Cream";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
+  { name: "Venusaur", height: 2, types: ["grass", "poison"] },
+];
