3bd0fd0660fcdf97390d7959e62840c50c64f899
Liz <51245710+eahowell@users.noreply.github.com>
2023-12-12 17:04:04 -0600

    Update: Moved pokemonList into IIFE and added validation
    Moved the pokemonList into an IIFE and created functions to return the entire repository and to add a new item. Also added validation to make sure the item was an object and had the correct keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,77 @@
-// Create the array of Pokemons
-let pokemonList = [];
-pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
-  { name: "Venusaur", height: 2, types: ["grass", "poison"] },
-];
-console.log(pokemonList);
+let pokemonRepository = (function () {
+  // Create the array of Pokemons
+  let pokemonList = [];
+  pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
+    { name: "Venusaur", height: 2, types: ["grass", "poison"] },
+  ];
+  let keysPokemonList = ["name", "height", "types"];
+
+  // Compare two arrays
+  function compareArrays(array1, array2) {
+    if (
+      array1.length === array2.length &&
+      array1.every((element, index) => element === array2[index])
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function add(pokemon) {
+    // Verify the pokemon being passed is an object
+    if (typeof pokemon === "object") {
+      let keyCheck = Object.keys(pokemon);
+      // Verify the pokemon being passed the keys matched the required keys
+      if (compareArrays(keyCheck, keysPokemonList)) {
+        pokemonList.push(pokemon);
+      } else {
+        alert(
+          "Keys did not match the Pokemon being added, please make sure you follow the proper formoat ['name', 'height', 'types']"
+        );
+      }
+    } else {
+      alert("Item being added is not an object.");
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+// Add a pokemon to the repository
+// console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Charmander", height: 0.6, types: ["fire"] });
 
 // Set up table header
 document.write(`
-    <table class=tablePokemonList>
-    <thead>
-        <tr>
-            <th> Name </th>
-            <th> Height (m) </th>
-            <th> Type </th>
-            <th> Size </th>
-        </tr>
-    </thead>
-    <tbody>
-    `);
+      <table class=tablePokemonList>
+      <thead>
+          <tr>
+              <th> Name </th>
+              <th> Height (m) </th>
+              <th> Type </th>
+              <th> Size </th>
+          </tr>
+      </thead>
+      <tbody>
+      `);
 
 // Loop through array to put the information in a table
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   document.write(`<tr>
-        <td>${pokemon.name}</td>
-        <td>${pokemon.height}</td>   
-        <td>${pokemon.types}</td>
-        <td>
-        `);
+          <td>${pokemon.name}</td>
+          <td>${pokemon.height}</td>   
+          <td>${pokemon.types}</td>
+          <td>
+          `);
 
   // Determine the size classification based on the height
   if (pokemon.height < 1) {
@@ -42,12 +85,10 @@ pokemonList.forEach(function(pokemon) {
   }
 
   document.write(`
-            </td>  
-        </tr>`);
+              </td>  
+          </tr>`);
 });
 document.write(`
-        </tbody>
-        </table>
-    `);
-
-
+          </tbody>
+          </table>
+      `);
