393b383ce121fb661e1df5825d030d553546dd97
Liz <51245710+eahowell@users.noreply.github.com>
2023-12-17 17:48:12 -0600

    InProgressChange: Working on Filter option
    I tried several different ways to try and get the filter working, but couldn't quite get the syntax right.
    I've commented out at the bottom of my js file if you can take a look and let me know if I was on the right track or not even in the ball park.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// https://pokedex.org/#/
 let pokemonRepository = (function () {
   // Create the array of Pokemons
   let pokemonList = [];
@@ -38,9 +39,36 @@ let pokemonRepository = (function () {
 // console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: "Charmander", height: 0.6, types: ["fire"] });
 
+// Compare two arrays
+function compareArrays(array1, array2) {
+  if (
+    array1.length === array2.length &&
+    array1.every((element, index) => element === array2[index])
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Populate dropdown
+pokemonRepository.getAll().forEach(function (pokemon) {
+  var optionsPokemon = document.createElement("option");
+  optionsPokemon.value = pokemon.name;
+  optionsPokemon.text = pokemon.name;
+  pokemonsChoice.add(optionsPokemon);
+});
+
+function setChoice() {
+  let choice = document.getElementById("pokemonsChoice").value;
+  choice.value = this.name;
+  choice.innerText = this.name;
+  console.log(choice);
+}
+
 // Set up table header
 document.write(`
-      <table class=tablePokemonList>
+      <table class="tablePokemonList" id="tablePokemonList">
       <thead>
           <tr>
               <th> Name </th>
@@ -49,7 +77,7 @@ document.write(`
               <th> Size </th>
           </tr>
       </thead>
-      <tbody>
+      <tbody id="tablePokemonListBody">
       `);
 
 // Loop through array to put the information in a table
@@ -71,7 +99,6 @@ pokemonRepository.getAll().forEach(function (pokemon) {
   } else {
     document.write("Unclassified");
   }
-
   document.write(`
               </td>  
           </tr>`);
@@ -81,36 +108,68 @@ document.write(`
           </table>
       `);
 
-// Compare two arrays
-function compareArrays(array1, array2) {
-  if (
-    array1.length === array2.length &&
-    array1.every((element, index) => element === array2[index])
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// This below was my initial attempt at a few different ways to try to filter the table.
+// function setChoice () {
+//   let choice = document.getElementById("pokemonsChoice").value
+//   choice.value = this.name
+//   choice.innerText  = this.name
 
-// Filter list by dropdown
-document.write(`
-<form id="formFilterPokemon action="#">
-<label for="filterPokemon">Pokemon Filter</label>
-<select name="pokemonsFilterList" id="pokemonsFilterList">
-<option value="All">All</option>
-`)
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(`<option value="
-    ${pokemon.name}
-    ">
-    ${pokemon.name}
-    </option>
-    `
-  )
-    
-})
-document.write(`
-</select>
-<input type="submit" value="Submit" />
-</form>`)
+//   // let table = document.getElementById("tablePokemonList");
+//   // Get the table body element in which you want to add row
+//   let table = document.getElementById("tablePokemonListBody");
+
+//   // Create row element
+//   let row = document.createElement("tr");
+
+//   console.log(choice)
+//   if (
+//     choice.innerText === "All"
+//     ) {
+
+//       // Loop through array to put the information in a table
+//     pokemonRepository.getAll().forEach(function (pokemon) {
+//       // let row = table.insertRow(-1);
+//       let c1 = document.createElement("td");
+//       let c2 = document.createElement("td");
+//       let c3 = document.createElement("td");
+//       let c4 = document.createElement("td");
+
+//       c1.innerText = pokemon.name;
+//       c2.innerText = pokemon.height;
+//       c3.innerText = pokemon.types;
+//       row.appendChild(c1);
+//       row.appendChild(c2);
+//       row.appendChild(c3);
+//       row.appendChild(c4);
+//       // Append row to table body
+//       table.appendChild(row)
+//       // determineSize(pokemon, c4);
+//   })
+// } else {
+//   let filteredRepository = pokemonRepository.getAll().filter((pokemon) => pokemon.name = choice.name)
+//     // let row = table.insertRow(-1);
+//     let c1 = row.insertCell(0);
+//     let c2 = row.insertCell(1);
+//     let c3 = row.insertCell(2);
+//     let c4 = row.insertCell(3);
+
+//     c1.innerText = filteredRepository[0];
+//     c2.innerText = filteredRepository[1];
+//     c3.innerText = filteredRepository[2];
+//     // determineSize(pokemon, c4);
+//   }
+
+// };
+//   // Determine the size classification based on the height
+//   function determineSize (pokemon, c) {
+
+//   if (pokemon.height < 1) {
+//       c4.innerText = "Tiny";
+//   } else if (pokemon.height >= 1 && pokemon.height <= 1.9) {
+//     c4.innerText = "Medium";
+//   } else if (pokemon.height > 1.9) {
+//     c4.innerText = "Large";
+//   } else {
+//     c4.innerText ="Unclassified";
+//   }
+// }
